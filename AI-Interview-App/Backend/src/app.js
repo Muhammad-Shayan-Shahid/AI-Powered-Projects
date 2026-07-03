1e6c50fe6c798c75b0b7fbe7d4571288a950850c
Muhammad Shayan Shahid <msshahid2305006@gmail.com>
2026-07-03 19:18:04 +0500

    Dist Integration done

--- AI-Interview-App/Backend/src/app.js
+++ AI-Interview-App/Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(express.json())
-app.use(express.static('./public/dist'))
+app.use(express.static("./public/dist"))
 app.use('/api/auth', authRoutes);
 app.use('/api/session', sessionRoutes);
 app.use('/api/answer', answerRoutes);
